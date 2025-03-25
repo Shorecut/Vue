c1eb015582a125df1845a48dd5c572ae0c772f62
Shorecut <chermanderco@gmail.com>
2025-03-25 16:02:14 +0600

    transtion for post list

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div style="margin-top: 15px" v-if="posts.length > 0">
     <h2 style="text-align: center">Post List</h2>
-    <post-item
-      @remove="$emit('remove', post)"
-      :key="post.id"
-      v-for="post in posts"
-      :post="post"
-    />
+    <transition-group name="post-list" tag="div">
+      <post-item
+        @remove="$emit('remove', post)"
+        :key="post.id"
+        v-for="post in posts"
+        :post="post"
+      />
+    </transition-group>
   </div>
   <h2 v-else style="text-align: center; margin-top: 15px">NO Posts</h2>
 </template>
@@ -23,4 +25,22 @@ export default {
   },
 };
 </script>
-<style scoped></style>
+<style scoped>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(40px);
+}
+.post-list-move {
+  transition: transform 0.8s ease;
+}
+</style>
